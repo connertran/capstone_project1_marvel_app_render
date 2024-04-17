9db1dd43b7c8e2b652057de44affde85570521fb
conner <conner.tran@bsa.edu.com>
2024-04-17 14:04:36 +0200

    cleaning code

---
+++ secret_info.py
@@ -0,0 +1,8 @@
+#https://developer.marvel.com/account
+api_public_key = '5fccb711370d4485bc52b0cfebf93a27'
+api_private_key = 'de1393e8add78f7fe182d1455bb5691958b63d48'
+ts = "conner123"
+
+# use: https://www.md5hashgenerator.com/
+# ts+ private_key + public_key => MD5 hash
+hashVal= "6eb4be2d7bcb5551348fd16f57de7a75"
